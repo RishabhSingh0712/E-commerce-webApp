ade4dd4288cadf7b172378a1ff7ca89c15bf45bc
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-04-13 16:06:59 +0530

    add schema

--- index.js
+++ index.js
@@ -15,18 +15,59 @@ db.on("error",()=>{
 db.once ("open",()=>{
     console.log("DB connected sucessfully")
      //logic to insert data into the DB
-    xyz()
+    // xyz()
+    //running the queries on MongoDB
+    dbQueries()
 })
 
- async function xyz() {
-    //logic to insert data into the DB
-    const students = {
-        name: 'raunak',
-        age: 40,
-        email: "rishabh@gmail.com",
-        subjects:["FSWD", "DS","DSA"]
-    }
+//  async function xyz() {
+//     //logic to insert data into the DB
+//     const students = {
+//         name: 'raunak',
+//         age: 40,
+//         email: "rishabh@gmail.com",
+//         subjects:["FSWD", "DS","DSA"]
+//     }
 
-  const std_obj = await studentModel.create(students)
-  console.log(std_obj)
+//   const std_obj = await studentModel.create(students)
+//   console.log(std_obj)
+// }
+
+async function dbQueries() {
+    // add logic to read the data 
+
+    //i want to go and search based on user_id
+
+  try {
+     const student = await studentModel.findById("661a2ecf4ef03f29cb71f5b9")
+     console.log(student)
+  } catch (error) {
+    console.log(err)
+  }
+
+// i want to go and search based on name 
+try {
+    const students = await studentModel.find({name:"raunak"})
+    console.log(students)
+} catch (error) {
+    console.log(err)
+}
+// i want to go and search based on email
+try {
+    const student = await studentModel.find({email:"rishabh@gmail.com"})
+    console.log(student)
+} catch (error) {
+    console.log(err)
+}
+
+/**
+ * if you are enterd empty find() then return all the data through DB
+ */
+
+// deal with multiple conditions
+
+const stds = await studentModel.where("age").gt("10").where("name").equals("raunak").limit(2)
+console.log(stds)
+
+
 }
