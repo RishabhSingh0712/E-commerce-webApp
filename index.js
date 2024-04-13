9e97278abe01c2400977e5bd6e1e0dc191c49771
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-04-13 12:40:54 +0530

    Add DB and save the details in DB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // import mongoose from "mongoose";
  const mongoose = require("mongoose")
+ const studentModel = require("./model/student.model")
 // write the code to connect with MongoDB
 
 mongoose.connect("mongodb://localhost/be_demo")
@@ -8,7 +9,24 @@ const db = mongoose.connection // start the connection with mongodb
 
 db.on("error",()=>{
     console.log("DB connection failed")
+   
+    
 })
 db.once ("open",()=>{
     console.log("DB connected sucessfully")
+     //logic to insert data into the DB
+    xyz()
 })
+
+ async function xyz() {
+    //logic to insert data into the DB
+    const students = {
+        name: 'raunak',
+        age: 40,
+        email: "rishabh@gmail.com",
+        subjects:["FSWD", "DS","DSA"]
+    }
+
+  const std_obj = await studentModel.create(students)
+  console.log(std_obj)
+}
