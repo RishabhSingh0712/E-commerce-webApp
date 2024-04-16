cf86408edf8d6851dc029b0eea1751d9ed0d7476
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-04-16 15:09:30 +0530

    add model

---
+++ demo/student.model.js
@@ -0,0 +1,29 @@
+// defiend the schema of students collection to be stored in the DB
+
+const mongoose = require ("mongoose")
+
+// schema 
+
+const studentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age:{
+        type: Number,
+        required: true ,
+        min: 18
+    },
+    email:{
+        type: String,
+        required: true,
+        lowercase: true,
+        minLength:15
+    },
+    subjects:[String]
+
+},{versionKey : false , timestamps : true})
+
+//Go ahead and create corresponding collections in DB
+
+module.exports = mongoose.model("Student",studentSchema)
