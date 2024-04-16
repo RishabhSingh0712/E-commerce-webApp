05d503fa403802aca99198477ca04bdfa88197e7
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-04-16 17:08:57 +0530

    add server

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const express = require ("express")
 const mongoose = require ("mongoose")
 const app = express()
 const server_config = require("./configs/server.configs.js")
+const db_config = require("./configs/db.configs.js")
+const user_model = require ("./models/user.model.js")
+const bcrypt = require("bcryptjs")
 
 
 /**
@@ -13,8 +16,38 @@ const server_config = require("./configs/server.configs.js")
  */
 
 // connection with mongoDB
-mongoose.connect()
+mongoose.connect(db_config.DB_URL)
 
+const db = mongoose.connection
+ 
+db.on("error",()=>{
+console.log("DB Connection Failed")
+})
+db.once("open", ()=>{
+    console.log("DB Connected Sucessfully")
+    init()
+})
+
+async function init() {
+    let user = await user_model.findOne({userId: "admin1"})
+    if (user) {
+        console.log ("admin is present")
+        return
+    }
+    try {
+        user = await user_model.create({
+            name:"rishabh singh",
+            userId:"admin1",
+            email:"rishabh@test.com",
+            userType:"ADMIN",
+            password:bcrypt.hashSync("tessst01",8)
+        })
+        console.log("Admin created", user)
+
+    } catch (error) {
+        console.log("error while creating the admin", err)
+    }
+}
 
 /**
  * start the server
