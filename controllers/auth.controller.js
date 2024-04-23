48b4797a0e8713e77098818b7f39580116b25642
Rishabh singh <125394319+RishabhSingh0712@users.noreply.github.com>
2024-04-23 17:49:31 +0530

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,50 +1,79 @@
 /**
  * I need to write the controller/logic to register a user
  */
-const bcrypt = require ("bcryptjs")
-const user_model = require("../models/user.model")
+const bcrypt = require("bcryptjs");
+const user_model = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+const secret = require("../configs/auth.configs");
 
-exports.signup= async(req,res,next)=>{
+// signup code
+
+exports.signup = async (req, res, next) => {
+  /**
+   * Logic to create the user
+   */
+
+  //1. Read the request body
+  const request_body = req.body;
+
+  //2. Insert the data in the users collections in mongoDB
+  const userObj = {
+    name: request_body.name,
+    userId: request_body.userId,
+    email: request_body.email,
+    userType: request_body.userType,
+    password: bcrypt.hashSync(request_body.password),
+  };
+  try {
+    const user_created = await user_model.create(userObj);
     /**
-     * Logic to create the user
+     * return the user
      */
+    const res_obj = {
+      name: user_created.name,
+      userId: user_created.userId,
+      email: user_created.email,
+      userType: user_created.userType,
+      createdAt: user_created.createdAt,
+      updateAt: user_created.updatedAt,
+    };
+    res.status(201).send(res_obj);
+  } catch (error) {
+    console.log("Error while registering the user");
+    res.status(500).send({
+      message: "some error happened while registering the user",
+    });
+  }
+};
 
-    //1. Read the request body
-    const request_body = req.body
+//signin code
 
-    //2. Insert the data in the users collections in mongoDB
-    const userObj = {
-        name : request_body.name,
-        userId : request_body.userId,
-        email : request_body.email,
-        userType : request_body.userType,
-        password : bcrypt.hashSync(request_body.password)
+exports.signin = async (req, res) => {
+  // check if the user id is present in the system or not
+  const user = await user_model.findOne({ userId: req.body.userId });
+  if (user == null) {
+    res.status(400).send({
+      message: "User id passed is not a valid user Id",
+    });
+  }
+  //password is correct
+  const isPasswordValid = bcrypt.compareSync(req.body.password, user.password);
+  if (!isPasswordValid) {
+    res.status(401).send({
+      message: "Wrong password passed",
+    });
+  }
 
-    }
-        try {
-        const user_created = await user_model.create(userObj);
-        /**
-         * return the user
-         */
-        const res_obj = {
-            name: user_created.name,
-            userId : user_created.userId,
-            email: user_created.email,
-            userType: user_created.userType,
-            createdAt: user_created.createdAt,
-            updateAt: user_created.updatedAt
-        }
-        res.status(201).send (res_obj)
+  //using JWT we will create the access token with a given TTL and return it
 
-    } catch (error) {
-        console.log("Error while registering the user");
-        res.status(500).send({
-            message: "some error happened while registering the user"
-        })
-    }
-    
-}
-
-exports.login = async(req,res)=>{
-    
-}
+  const token = jwt.sign({ id: user.userId }, secret.secret, {
+    expiresIn: 120,
+  });
+  res.status(200).send({
+    name: user.name,
+    userId: user.userId,
+    email: user.email,
+    userType: user.userType,
+    accessToken: token,
+  });
+};
